2ede953d07d7554d2ee70498778e57eefc9cdaed
rhaoio <richard@rhao.io>
2024-03-01 18:00:29 +1100

    remove unused for keccak

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,5 @@ export const config = {
   scrollRpcUrl: "https://sepolia-rpc.scroll.io",
   zkWasmExplorerUrl: "http://138.217.142.94:8092",
   sepoliaRpcUrl: "https://rpc.sepolia.org",
-  sepoliaContractAddressB: "",
-  scrollContractAddressC: "",
   privateKey: "",
 };
